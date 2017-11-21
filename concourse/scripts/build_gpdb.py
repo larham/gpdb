6753c26e0e302c18ceea52c30b879e5017d3b3e6
Larry Hamel <lhamel@pivotal.io>
2017-11-21 14:02:58 -0800

    fix status checking on installcheck
    - also refactor common block out of conditional
    - Also, update README.md
    
    Signed-off-by: Goutam Tadi <gtadi@pivotal.io>

--- concourse/scripts/build_gpdb.py
+++ concourse/scripts/build_gpdb.py
@@ -72,8 +72,8 @@ def main():
     parser.add_option("--compiler", dest="compiler")
     parser.add_option("--cxxflags", dest="cxxflags")
     parser.add_option("--output_dir", dest="output_dir", default=INSTALL_DIR)
-    parser.add_option("--configure-option", dest="configure_option", action="append", help="Configure flags, \
-                                                                                            ex --configure_option=--disable-orca --configure_option=--disable-gpcloud")
+    parser.add_option("--configure-option", dest="configure_option", action="append",
+                      help="Configure flags, ex --configure_option=--disable-orca --configure_option=--disable-gpcloud")
     parser.add_option("--gcc-env-file", dest="gcc_env_file", help="GCC env file to be sourced")
     parser.add_option("--orca-in-gpdb-install-location", dest="orca_in_gpdb_install_location", action="store_true",
                       help="Install ORCA header and library files in GPDB install directory")
@@ -82,26 +82,23 @@ def main():
     parser.add_option("--gpdb_name", dest="gpdb_name")
     (options, args) = parser.parse_args()
 
-    ci_common = GpBuild(options.mode)
+    gpBuild = GpBuild(options.mode)
     status = print_compiler_version()
     fail_on_error(status)
 
     # optional gcc env file to be source before executing configure, make, make install commands
-    ci_common.set_gcc_env_file(options.gcc_env_file)
+    gpBuild.set_gcc_env_file(options.gcc_env_file)
 
-    # for building gpdb, orca and xerces library & header should be available
-    # if required install the files in the install directory of gpdb to avoid packaging from multiple directories
     install_dir = INSTALL_DIR if options.orca_in_gpdb_install_location else DEPENDENCY_INSTALL_DIR
-    if options.action == 'build':
-        install_dependencies(ci_common, args, install_dir)
-    # for testing, install the gpdb package assuming it has all the required dependencies
-    elif options.action == 'test':
-        # install gpdb
-        status = ci_common.install_dependency(options.gpdb_name, INSTALL_DIR)
+    if options.action == 'test':
+        # if required, install orca and xerces library & header
+        # in the install directory of gpdb to avoid packaging from multiple directories
+        status = gpBuild.install_dependency(options.gpdb_name, INSTALL_DIR)
         fail_on_error(status)
-        # if dependency available as args, install them
-        status = install_dependencies(ci_common, args, install_dir)
-        fail_on_error(status)
+
+    # install any dependencies specified on the command line
+    status = install_dependencies(gpBuild, args, install_dir)
+    fail_on_error(status)
 
     configure_option = []
     if options.configure_option:
@@ -112,20 +109,20 @@ def main():
         '"--with-libs={0} {1}"'.format(os.path.join(DEPENDENCY_INSTALL_DIR, "lib"), os.path.join(INSTALL_DIR, "lib")))
     configure_option.append('"--with-includes={0} {1}"'.format(os.path.join(DEPENDENCY_INSTALL_DIR, "include"),
                                                                os.path.join(INSTALL_DIR, "include")))
-    ci_common.append_configure_options(configure_option)
+    gpBuild.append_configure_options(configure_option)
 
-    status = ci_common.configure()
+    status = gpBuild.configure()
     fail_on_error(status)
 
     # compile and install gpdb
     if options.action == 'build':
-        status = ci_common.make()
+        status = gpBuild.make()
         fail_on_error(status)
 
-        status = ci_common.unittest()
+        status = gpBuild.unittest()
         fail_on_error(status)
 
-        status = ci_common.make_install()
+        status = gpBuild.make_install()
         fail_on_error(status)
 
         status = copy_installed(options.output_dir)
@@ -135,12 +132,12 @@ def main():
         status = create_gpadmin_user()
         fail_on_error(status)
         if os.getenv("TEST_SUITE", "icg") == 'icw':
-            status = ci_common.install_check('world')
+            status = gpBuild.install_check('world')
         else:
-            status = ci_common.install_check()
-            if status:
-                copy_output()
-            return status
+            status = gpBuild.install_check()
+        if status:
+            copy_output()
+        return status
 
     return 0
 
